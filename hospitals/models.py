f378a6bc2225cc83aff8cc053785f388f50fb9fc
ochiengowino <benjaminochieng99@gmail.com>
2024-11-16 14:34:03 +0300

    imported gis data

--- hospitals/models.py
+++ hospitals/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+# from django.db import models
+from django.contrib.gis.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Hospitals(models.Model):
+    name = models.CharField(_("Hospital Name"), max_length=100)
+    lon = models.FloatField(_("Longitude"))
+    lat = models.FloatField(_("Latitude"))
+    fid = models.IntegerField(_("Field ID"))
+    beds = models.IntegerField(_("Bed Numbers"), default=1)
+    province_name = models.CharField(_("Province"), max_length=100)
+    province_code = models.CharField(_("Province Code"), max_length=1)
+
+    geom = models.PointField(srid=4326)
+
+    def __str__(self):
+        return self.name
